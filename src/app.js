0a336c071fa9604d008633b36f7c22fb0df99248
Amit Rawat <rawatamit302003@gmail.com>
2025-04-08 15:04:10 +0530

    added dummy data to DB with post API call

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
-const express= require("express");
-const {connectDB}= require("./config/database");
-
-const app=express();
+const express = require("express");
+const { connectDB } = require("./config/database");
+const User = require("./models/User");
+const app = express();
 
 // app.get("/hey",
 //     (req,res,next)=>{
@@ -11,15 +11,30 @@ const app=express();
 // (req,res)=>{
 //     res.send(`Han bhai bdia na!!`);
 // })
-connectDB()
-.then(()=>{
-    console.log(`DataBase connection stablished🔥`);
-    app.listen(3000,()=>{
-        console.log(`Server successfull listening on PORT:`+`3000`);
-        
-    });
 
-}).catch((err) =>{
-     console.error(`DB cn't be connected!!`);   
+app.post("/signup", async (req, res) => {
+  //create a instance of a user model
+  const user = new User({
+    firstName: "Amit",
+    lastName: "Rawat",
+    emailId: "rwt@gmail.com",
+    age: "22",
+  });
+  try {
+    await user.save();
+    res.send(`User added succesfully`);
+  } catch (error) {
+    res.status(400).send(`Error svaing the user data` + err.message);
+  }
 });
 
+connectDB()
+  .then(() => {
+    console.log(`DataBase connection stablished🔥`);
+    app.listen(3000, () => {
+      console.log(`Server successfull listening on PORT:` + `3000`);
+    });
+  })
+  .catch((err) => {
+    console.error(`DB cn't be connected!!`);
+  });
