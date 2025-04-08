8204fe073660cf10f1dd213f7e294c60c00d76ac
Amit Rawat <rawatamit302003@gmail.com>
2025-04-08 21:51:01 +0530

    made APIs for fetch update and delete

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ const { connectDB } = require("./config/database");
 const User = require("./models/User");
 const app = express();
 
+app.use(express.json()); // express middleware to parse json data in server coming from client
+
 // app.get("/hey",
 //     (req,res,next)=>{
 //     // res.send(`Hello from the server!! kya haal chaal`);
@@ -13,18 +15,51 @@ const app = express();
 // })
 
 app.post("/signup", async (req, res) => {
-  //create a instance of a user model
-  const user = new User({
-    firstName: "Amit",
-    lastName: "Rawat",
-    emailId: "rwt@gmail.com",
-    age: "22",
-  });
+  //creating dynamic instance of a user model
+  const user = new User(req?.body);
   try {
     await user.save();
     res.send(`User added succesfully`);
   } catch (error) {
-    res.status(400).send(`Error svaing the user data` + err.message);
+    res.status(400).send(`Error svaing the user data` + error);
+  }
+});
+
+// /feed api using get API Call -->to display all the users
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (error) {
+    res.status(404).send(`Something went wrong!`);
+  }
+});
+
+//  /delete to delete a user
+app.delete("/delete", async (req, res) => {
+  const userId = req.body?.userId;
+  try {
+    // await User.findByIdAndDelete({_id: userId});
+    await User.findByIdAndDelete(userId);
+    res.send(`User deleted successfully!`);
+  } catch (error) {
+    res.status(404).send(`Something went wrong`);
+  }
+});
+
+// update a user using patch
+app.patch("/user", async (req, res) => {
+  const userId = req.body?.userId;
+  const newData = req.body;
+  try {
+    //here 3rd param is optional just to examine
+    const user = await User.findByIdAndUpdate(userId, newData, {
+      returnDocument: "before",
+    });
+    console.log(user); //before update data on console
+    res.send(`Data Updated Succesfully`);
+  } catch (error) {
+    res.status(400).send(`Something went wrong`);
   }
 });
 
