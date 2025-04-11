d9df8c7dfd670f5e403294530497d2d9e5271843
Amit Rawat <rawatamit302003@gmail.com>
2025-04-11 23:00:43 +0530

    implemented fetching accepted connections of loggedInUser

--- src/routes/user.js
+++ src/routes/user.js
@@ -4,23 +4,61 @@ const ConnectionRequest = require("../models/connectionRequest");
 
 const userRouter = express.Router();
 
-userRouter.get("/user/requests-pending", userAuth, async (req,res) => {
+userRouter.get("/user/requests-pending", userAuth, async (req, res) => {
   try {
     const loggedInUser = req?.user;
 
     const connectionRequests = await ConnectionRequest.find({
       toUserId: loggedInUser._id,
       status: "interested",
-    }).populate("fromUserId", "firstName lastName age gender photoUrl about skills")
-
+    }).populate(
+      "fromUserId",
+      "firstName lastName age gender photoUrl about skills"
+    );
 
     res.json({
-        message: `Pending Requests fetched Succesfully`,
-        data : connectionRequests,
-    })
+      message: `Pending Requests fetched Succesfully`,
+      data: connectionRequests,
+    });
   } catch (error) {
     res.status(400).send(`ERROR: ` + error.message);
   }
+});
+
+userRouter.get("/user/connections", userAuth, async (req, res) => {
+  try {
+    const loggedInUser = req?.user._id;
+
+    //to fetch accepted connections 
+    const acceptedRequests = await ConnectionRequest.find({
+      $or: [
+        { toUserId: loggedInUser._id, status: "accepted" },
+        { fromUserId: loggedInUser._id, status: "accepted" },
+      ],
+    })
+      .populate(
+        "fromUserId",
+        "firstName lastName age gender photoUrl about skills"
+      )
+      .populate(
+        "toUserId",
+        "firstName lastName age gender photoUrl about skills"
+      );
+      //why 2 populates? cause loggedInUser could be either fromUserId or either toUserID
+
+    //for more simplified and only requirede data
+    const requiredData = acceptedRequests.map((row) =>{
+        if(row.fromUserId._id.toString() === loggedInUser._id.toString()){
+            return row.toUserId;
+        }
+        return row.fromUserId;
+    });
+
+    res.json({
+      message: `Connections Fetched Succesfully`,
+      data: requiredData,
+    });
+  } catch (error) {}
 });
 
 module.exports = userRouter;
