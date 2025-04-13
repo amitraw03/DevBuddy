b08d7a753a06a5e3a22e813fc45e39a221314103
Amit Rawat <rawatamit302003@gmail.com>
2025-04-13 18:45:11 +0530

    implemnted daisy UI & tailwind & setup routin using react-router & built Navbar & footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Body from "./Components/Body";
+import Login from "./Components/Login";
+import Profile from "./Components/profile";
 
 function App() {
-
   return (
     <>
+        <BrowserRouter basename="/">
+          <Routes>
+            {/* Body is a parent route & inside it childs, controlled by Outet */}
+            <Route path="/" element={<Body/>}> 
+               <Route path="/profile" element={<Profile/>} />
+               <Route path="/login" element={<Login/>} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+
       <h1 className="bg-amber-800">Vite + React</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
