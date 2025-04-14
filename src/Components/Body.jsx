49411b90de6873e826cb96713bd831f92c3d7f84
Amit Rawat <rawatamit302003@gmail.com>
2025-04-14 20:15:29 +0530

    Routes access permission logic

--- src/Components/Body.jsx
+++ src/Components/Body.jsx
@@ -1,16 +1,43 @@
-import React from 'react'
-import Navbar from './Navbar'
-import Footer from './Footer'
-import { Outlet } from 'react-router-dom'
+import React, { useEffect } from "react";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+import { Outlet, useNavigate } from "react-router-dom";
+import { BASE_URL } from "../utils/constants";
+import { useDispatch, useSelector } from "react-redux";
+import { addUser } from "../utils/userSlice";
+import axios from "axios";
 
 const Body = () => {
+  const userData = useSelector((store)=>store.user);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const fetchUser = async () => {
+    if(userData) return; // if already data in redux store , no need for API calling
+    try {
+      const res = await axios.get(BASE_URL + "/profile/view", {
+        withCredentials: true,
+      });
+      dispatch(addUser(res.data));
+    } catch (error) {
+      if(error.status === 401){  // if token expired or loggedOut
+         navigate("/login");
+      }
+      console.log(error);
+    }
+  };
+
+  useEffect(()=>{
+    fetchUser();
+  },[])
+
   return (
     <div>
-        <Navbar/>
-        <Outlet/>
-        <Footer/>
+      <Navbar />
+      <Outlet />
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default Body
+export default Body;
